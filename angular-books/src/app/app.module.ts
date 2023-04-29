80ec7754b3668299d65971e6c7a50c3f6de383f1
usman asghar <usmanasgh@live.com>
2023-04-29 16:11:00 +0100

    component + material design angular

--- angular-books/src/app/app.module.ts
+++ angular-books/src/app/app.module.ts
@@ -5,15 +5,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksListComponent } from './books/books-list/books-list.component';
+import { GenericListComponent } from './utilities/generic-list/generic-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';  
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksListComponent
+    BooksListComponent,
+    GenericListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-books/src/app/books/books-list/books-list.component.ts
+++ angular-books/src/app/books/books-list/books-list.component.ts
@@ -17,4 +17,11 @@ export class BooksListComponent implements OnInit {
 @Input() // MUA: input parameter is used to pass objects from <app-books-list [books]="CurrentBooks"></app-books-list>
 books; //MUA : Books are defined here, and we are passing values from app.component.ts
 
+//MUA : define remove funtion
+remove(index : number)
+{
+  // MUA : Implementation goes here
+  this.books.splice(index,1);
+}
+
 }
